fb151d1eb144db4c48bbe432c27e99f48039013d
Yann Lachever <lachevery@gmail.com>
2016-11-07 23:49:30 +0100

    First version of Server
    Simply create a Listening socket and wait for client’s connection

---
+++ src/server/HangmanServer.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+/**
+ *
+ * @author YannL
+ */
+public class HangmanServer {
+
+    // Help message to use the server
+    static final String USAGE = "java HangmanServer [port]";
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Port number of the ServerSocket
+        int port = 8080;
+
+        // Parse input arguments
+        if (args.length > 0) {
+            // Display help if asked
+            if (args[0].equalsIgnoreCase("-h") || args[0].equalsIgnoreCase("-help")) {
+                System.out.println(USAGE);
+                System.exit(1);
+            }
+            
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.err.println(USAGE);
+                System.exit(1);
+            }
+        }
+        
+        // Creation of a listening socket bound to the specific port
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+            while (true) {
+                try {
+                    // Wait for a client connection request
+                    Socket clientSocket = serverSocket.accept();
+                    // Communicate with a client via clientSocket
+                    System.out.println("Client connected");
+                    // Close the socket and wait for another connection
+                    clientSocket.close();
+                    System.out.println("Client deconnected");
+                } catch (SocketException e) {
+                    System.err.println(e);
+                }
+            }
+
+        } catch (IOException ex) {
+            System.err.println("Error while creating the server listening socket : " + ex);
+        }
+    }
+}
